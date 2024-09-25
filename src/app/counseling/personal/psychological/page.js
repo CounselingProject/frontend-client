2ecec74d9a84c43135d730a9ec8709e8c2db23a1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-25 09:53:26 +0900

    안쓰는 import 부분 삭제

--- src/app/counseling/personal/psychological/page.js
+++ src/app/counseling/personal/psychological/page.js
@@ -1,10 +1,9 @@
 'use client';
 import React from 'react';
-import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import Link from 'next/link';
 import MemberOnlyContainer from '@/member/containers/MemberOnlyContainer';
 import PersonalCounselingContainer from '@/counseling/ps/containers/PersonalCounselingContainer';
+
 const CounselingPage = () => {
   const { t } = useTranslation();
 
