43bc53bc6d30a0860ab1f8f24c06f4f03bac0d88
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-27 08:55:41 +0900

    개인 상담 기본 페이지 <OuterBox> 로 감싸 놓음

--- src/app/counseling/personal/page.js
+++ src/app/counseling/personal/page.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
 import Image from 'next/image';
+import { OuterBox } from '@/commons/components/LayoutBox';
 
 const PersonalContainer = styled.div`
   display: flex;
@@ -64,64 +65,64 @@ const CounselingPage = () => {
   const { t } = useTranslation();
 
   return (
-    <PersonalContainer>
-      <h1 className="p1">{t('개인 상담')}</h1>
-      <h3 className="p2">
-        {t(
-          '개인 상담은 교수 상담, 취업 상담, 심리 상담 3가지로 구분되며 신청 페이지의 달력에서 일자 및 시간을 선택하여 신청할 수 있습니다.',
-        )}
-      </h3>
+    <OuterBox>
+      <PersonalContainer>
+        <h1 className="p1">{t('개인 상담')}</h1>
+        <h3 className="p2">
+          {t(
+            '개인 상담은 교수 상담, 취업 상담, 심리 상담 3가지로 구분되며 신청 페이지의 달력에서 일자 및 시간을 선택하여 신청할 수 있습니다.',
+          )}
+        </h3>
+        <ul className="counseling_list">
+          <li>
+            <Link
+              href="/counseling/personal/professor"
+              className="counseling_item"
+            >
+              <Image
+                className="CounselingImage"
+                src={`/images/personalCounseling/PROFESSOR.png`}
+                alt="교수 상담"
+                width={350}
+                height={350}
+              />
+              <span className="counseling_text">{t('교수 상담 신청하기')}</span>
+            </Link>
+          </li>
 
-      <ul className="counseling_list">
-        <li>
-          <Link
-            href="/counseling/personal/professor"
-            className="counseling_item"
-          >
-            <Image
-              className="CounselingImage"
-              src={`/images/personalCounseling/PROFESSOR.png`}
-              alt="교수 상담"
-              width={350}
-              height={350}
-            />
-            <span className="counseling_text">{t('교수 상담 신청하기')}</span>
-          </Link>
-        </li>
-
-        <li>
-          <Link
-            href="/counseling/personal/employment"
-            className="counseling_item"
-          >
-            <Image
-              className="CounselingImage"
-              src={`/images/personalCounseling/EMPLOYMENT.png`}
-              alt="취업 상담"
-              width={350}
-              height={350}
-            />
-            <span className="counseling_text">{t('취업 상담 신청하기')}</span>
-          </Link>
-        </li>
-
-        <li>
-          <Link
-            href="/counseling/personal/psychological"
-            className="counseling_item"
-          >
-            <Image
-              className="CounselingImage"
-              src={`/images/personalCounseling/PSYCHOLOGY.png`}
-              alt="심리 상담"
-              width={350}
-              height={350}
-            />
-            <span className="counseling_text">{t('심리 상담 신청하기')}</span>
-          </Link>
-        </li>
-      </ul>
-    </PersonalContainer>
+          <li>
+            <Link
+              href="/counseling/personal/employment"
+              className="counseling_item"
+            >
+              <Image
+                className="CounselingImage"
+                src={`/images/personalCounseling/EMPLOYMENT.png`}
+                alt="취업 상담"
+                width={350}
+                height={350}
+              />
+              <span className="counseling_text">{t('취업 상담 신청하기')}</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/counseling/personal/psychological"
+              className="counseling_item"
+            >
+              <Image
+                className="CounselingImage"
+                src={`/images/personalCounseling/PSYCHOLOGY.png`}
+                alt="심리 상담"
+                width={350}
+                height={350}
+              />
+              <span className="counseling_text">{t('심리 상담 신청하기')}</span>
+            </Link>
+          </li>
+        </ul>
+      </PersonalContainer>
+    </OuterBox>
   );
 };
 
