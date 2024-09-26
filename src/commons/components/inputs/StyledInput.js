2f817bef879b3b88f77c0e42835b16395c57a3be
lionjeongin <cindy93489163@gmail.com>
2024-09-26 16:45:00 +0900

    .

--- src/commons/components/inputs/StyledInput.js
+++ src/commons/components/inputs/StyledInput.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 export const StyledInput = styled.input`
-    border: 1px solid ${({ theme }) => theme.colors.gray};
-    padding: 10px;
-    width: 100%;
-    border: 1px solid #d9d8d8;
-    height: 40px;
-    font-size: 14px;
-    margin-bottom: 3px;
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  padding: 10px;
+  width: 100%;
+  border: 1px solid #d9d8d8;
+  height: 40px;
+  font-size: 14px;
+  margin-bottom: 3px;
 `;
 
 export const StyledTextarea = styled.textarea`
@@ -18,3 +18,5 @@ export const StyledTextarea = styled.textarea`
   min-height: 250px;
   resize: none;
 `;
+
+export const StyledSelect = styled.select``;
