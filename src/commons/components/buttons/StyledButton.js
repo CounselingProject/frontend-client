f067b80103ea26bb4a9f3aa2b199ca93059e8a67
parksaehyun <psh666777@naver.com>
2024-09-26 16:15:09 +0900

    style : 마이페이지 스타일 작업 중 fix : 지도교수 직렬화 문제 해결함

--- src/commons/components/buttons/StyledButton.js
+++ src/commons/components/buttons/StyledButton.js
@@ -38,7 +38,7 @@ export const MypageStyledButton = styled.button`
         height = height ?? '38px';
         return css`
       color: #fff;
-      background-color: #004b5e;
+      background-color: ${theme.colors.green};
       border: ${border};
       font-size: ${theme.fontSizes[size] || '14px'};
       width: ${width};
@@ -55,7 +55,7 @@ export const MypageStyledButton = styled.button`
 
     // 호버 시 배경 색상 변경
     &:hover {
-        background-color: #006a7f; /* 원하는 색상으로 변경 */
+        background-color: #005d4f; /* 원하는 색상으로 변경 */
         opacity: 0.9; /* 살짝 투명하게 효과 추가 가능 */
     }
 `;
--- src/mypage/components/ProfileUpdate.js
+++ src/mypage/components/ProfileUpdate.js
@@ -9,28 +9,44 @@ import { MypageStyledButton } from '@/commons/components/buttons/StyledButton';
 import StyledMessage from '@/commons/components/StyledMessage';
 import userType from '@/member/constants/userType';
 import FileUpload from '@/commons/components/FileUpload';
+import { StyledH1 } from '@/commons/components/Mypage/StyledH1';
+
+// 전체 레이아웃을 위한 스타일드 컴포넌트
+const Container = styled.div`
+    display: flex; /* 좌우로 나누기 위해 flexbox 사용 */
+    justify-content: space-between; /* 좌우 공간을 고르게 분배 */
+    gap: 20px; /* 두 섹션 간의 간격 */
+`;
+
+const LeftSection = styled.div`
+    flex: 1; /* 왼쪽 섹션 크기 조정 */
+`;
+
+const RightSection = styled.div`
+    flex: 1; /* 오른쪽 섹션 크기 조정 */
+`;
 
 // StyledFileUpload 정의
 const StyledFileUpload = styled(FileUpload)`
-    display: flex; /* flexbox를 사용하여 이미지 중앙 정렬 */
-    align-items: center; /* 세로 중앙 정렬 */
-    justify-content: center; /* 가로 중앙 정렬 */
-    border-radius: 50%; /* 원형 테두리 */
-    border: 2px solid ${({ theme }) => theme.colors.primary}; /* 테두리 색상 */
-    overflow: hidden; /* 이미지가 테두리 영역을 넘어가지 않도록 설정 */
-    width: 100px; /* 너비 */
-    height: 100px; /* 높이 */
-    background-color: ${({ theme }) => theme.colors.gray}; /* 배경색 */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid ${({ theme }) => theme.colors.white};
+    overflow: hidden;
+    width: 100px;
+    height: 100px;
+    background-color: ${({ theme }) => theme.colors.white};
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
     img {
-        width: 100%; /* 이미지 너비 */
-        height: 100%; /* 이미지 높이 */
-        object-fit: cover; /* 이미지 비율 유지 */
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
 
     &:hover {
-        border-color: ${({ theme }) => theme.colors.secondary}; /* 호버 시 테두리 색상 변화 */
+        border-color: ${({ theme }) => theme.colors.white};
     }
 `;
 
@@ -47,144 +63,159 @@ const ProfileUpdate = ({
 
   return (
     <FormBox onSubmit={onSubmit} autoComplete="off">
-      <dl>
-        <StyledDt>{t('프로필_이미지')}</StyledDt>
-        <StyledFileUpload
-          imageOnly={true}
-          gid={form?.gid}
-          single={true}
-          done={true}
-          width={400}
-          color="primary"
-          imageUrl={form?.profileImage}
-          callback={fileUploadCallback}
-        >
-          {t('이미지_첨부')}
-        </StyledFileUpload>
-      </dl>
-      <dl>
-        <StyledDt>{t('회원유형')}</StyledDt>
-        <StyledDd>{userType[form?.userType]}</StyledDd>
-      </dl>
-      <dl>
-        <StyledDt>{t('이메일')}</StyledDt>
-        <StyledDd>{form?.email}</StyledDd>
-      </dl>
-      <dl>
-        <StyledDt>{t('회원명')}</StyledDt>
-        <StyledDd>{form?.userName}</StyledDd>
-      </dl>
-      <dl>
-        <StyledDt>{t('비밀번호')}</StyledDt>
-        <StyledInput
-          type="password"
-          name="password"
-          value={form?.password ?? ''}
-          onChange={onChange}
-        />
-        <StyledMessage variant="danger">{errors?.password}</StyledMessage>
-      </dl>
-      <dl>
-        <StyledDt>{t('비밀번호_확인')}</StyledDt>
-        <StyledInput
-          type="password"
-          name="confirmPassword"
-          value={form?.confirmPassword ?? ''}
-          onChange={onChange}
-        />
-        <StyledMessage variant="danger">
-          {errors?.confirmPassword}
-        </StyledMessage>
-      </dl>
-      <dl>
-        <StyledDt>{t('휴대전화번호')}</StyledDt>
-        <StyledInput
-          type="text"
-          name="mobile"
-          value={form?.mobile ?? ''}
-          onChange={onChange}
-        />
-        <StyledMessage variant="danger">{errors?.mobile}</StyledMessage>
-      </dl>
-      <dl>
-        <StyledDt>{t('우편번호')}</StyledDt>
-        <StyledInput
-          type="text"
-          name="zonecode"
-          value={form?.zonecode ?? ''}
-          onChange={onChange}
-        />
-        <StyledMessage variant="danger">{errors?.zonecode}</StyledMessage>
-      </dl>
-      <dl>
-        <StyledDt>{t('주소')}</StyledDt>
-        <StyledInput
-          type="text"
-          name="address"
-          value={form?.address ?? ''}
-          onChange={onChange}
-        />
-        <StyledMessage variant="danger">{errors?.address}</StyledMessage>
-      </dl>
-      <dl>
-        <StyledDt>{t('나머지_주소')}</StyledDt>
-        <StyledInput
-          type="text"
-          name="addresssub"
-          value={form?.addresssub ?? ''}
-          onChange={onChange}
-        />
-        <StyledMessage variant="danger">{errors?.addresssub}</StyledMessage>
-      </dl>
-      <dl>
-        <StyledDt>{t('생년월일')}</StyledDt>
-        <StyledDd>{form?.birth}</StyledDd>
-      </dl>
-      <dl>
-        <StyledDt>{t('성별')}</StyledDt>
-        <StyledDd>{form?.gender === 'FEMALE' ? t('여성') : t('남성')}</StyledDd>
-      </dl>
+      <StyledH1>{t('개인정보_수정')}</StyledH1>
 
-      {form?.userType === 'STUDENT' ? (
-        <>
           <dl>
-            <StyledDt>{t('학번')}</StyledDt>
-            <StyledDd>{form?.stdntNo}</StyledDd>
+            <StyledDt>{t('프로필_이미지')}</StyledDt>
+            <StyledFileUpload
+              imageOnly={true}
+              gid={form?.gid}
+              single={true}
+              done={true}
+              width={400}
+              color="primary"
+              imageUrl={form?.profileImage}
+              callback={fileUploadCallback}
+            >
+              {t('이미지_첨부')}
+            </StyledFileUpload>
+          </dl>
+      <Container>
+        <LeftSection>
+          <dl>
+            <StyledDt>{t('이름')}</StyledDt>
+            <StyledDd>{form?.userName}</StyledDd>
           </dl>
           <dl>
-            <StyledDt>{t('학년')}</StyledDt>
-            <StyledDd>{form?.grade}</StyledDd>
+            <StyledDt>{t('회원유형')}</StyledDt>
+            <StyledDd>{userType[form?.userType]}</StyledDd>
           </dl>
-        </>
-      ) : (
-        <>
+          {/* STUDENT / EMPLOYEE에 따른 추가 입력 */}
+          {form?.userType === 'STUDENT' ? (
+            <>
+              <dl>
+                <StyledDt>{t('지도교수')}</StyledDt>
+                <StyledDd>{form?.professor.userName}</StyledDd>
+              </dl>
+              <dl>
+                <StyledDt>{t('학번')}</StyledDt>
+                <StyledDd>{form?.stdntNo}</StyledDd>
+              </dl>
+              <dl>
+                <StyledDt>{t('학년')}</StyledDt>
+                <StyledDd>{form?.grade}</StyledDd>
+              </dl>
+            </>
+          ) : (
+            <>
+              <dl>
+                <StyledDt>{t('사번')}</StyledDt>
+                <StyledInput
+                  type="text"
+                  name="empNo"
+                  value={form?.empNo ?? ''}
+                  onChange={onChange}
+                />
+                <StyledMessage variant="danger">{errors?.empNo}</StyledMessage>
+              </dl>
+              <dl>
+                <StyledDt>{t('담당 과목')}</StyledDt>
+                <StyledInput
+                  type="text"
+                  name="subject"
+                  value={form?.subject ?? ''}
+                  onChange={onChange}
+                />
+                <StyledMessage variant="danger">{errors?.subject}</StyledMessage>
+              </dl>
+            </>
+          )}
+
           <dl>
-            <StyledDt>{t('사번')}</StyledDt>
+            <StyledDt>{t('이메일')}</StyledDt>
+            <StyledDd>{form?.email}</StyledDd>
+          </dl>
+          <dl>
+            <StyledDt>{t('생년월일')}</StyledDt>
+            <StyledDd>{form?.birth}</StyledDd>
+          </dl>
+        </LeftSection>
+
+        <RightSection>
+          <dl>
+            <StyledDt>{t('성별')}</StyledDt>
+            <StyledDd>{form?.gender === 'FEMALE' ? t('여성') : t('남성')}</StyledDd>
+          </dl>
+          <dl>
+            <StyledDt>{t('비밀번호')}</StyledDt>
             <StyledInput
-              type="text"
-              name="empNo"
-              value={form?.empNo ?? ''}
+              type="password"
+              name="password"
+              value={form?.password ?? ''}
               onChange={onChange}
             />
-            <StyledMessage variant="danger">{errors?.empNo}</StyledMessage>
+            <StyledMessage variant="danger">{errors?.password}</StyledMessage>
           </dl>
           <dl>
-            <StyledDt>{t('담당 과목')}</StyledDt>
+            <StyledDt>{t('비밀번호_확인')}</StyledDt>
             <StyledInput
-              type="text"
-              name="subject"
-              value={form?.subject ?? ''}
+              type="password"
+              name="confirmPassword"
+              value={form?.confirmPassword ?? ''}
               onChange={onChange}
             />
-            <StyledMessage variant="danger">{errors?.subject}</StyledMessage>
+            <StyledMessage variant="danger">
+              {errors?.confirmPassword}
+            </StyledMessage>
           </dl>
-        </>
-      )}
+          <dl>
+            <StyledDt>{t('휴대전화번호')}</StyledDt>
+            <StyledInput
+              type="text"
+              name="mobile"
+              value={form?.mobile ?? ''}
+              onChange={onChange}
+            />
+            <StyledMessage variant="danger">{errors?.mobile}</StyledMessage>
+          </dl>
+          <dl>
+            <StyledDt>{t('우편번호')}</StyledDt>
+            <StyledInput
+              type="text"
+              name="zonecode"
+              value={form?.zonecode ?? ''}
+              onChange={onChange}
+            />
+            <StyledMessage variant="danger">{errors?.zonecode}</StyledMessage>
+          </dl>
+          <dl>
+            <StyledDt>{t('주소')}</StyledDt>
+            <StyledInput
+              type="text"
+              name="address"
+              value={form?.address ?? ''}
+              onChange={onChange}
+            />
+            <StyledMessage variant="danger">{errors?.address}</StyledMessage>
+          </dl>
+          <dl>
+            <StyledDt>{t('세부_주소')}</StyledDt>
+            <StyledInput
+              type="text"
+              name="addresssub"
+              value={form?.addresssub ?? ''}
+              onChange={onChange}
+            />
+            <StyledMessage variant="danger">{errors?.addresssub}</StyledMessage>
+          </dl>
+        </RightSection>
+      </Container>
 
       <MypageStyledButton type="submit" variant="primary">
         {t('회원정보 수정')}
       </MypageStyledButton>
       <StyledMessage variant="danger">{errors?.global}</StyledMessage>
+
     </FormBox>
   );
 };
