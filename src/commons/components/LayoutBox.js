188f9ded51ccf9d5603165a15e1a5e7d90ec7bfd
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-27 12:24:59 +0900

    달력 페이지 최종 수정

--- src/commons/components/LayoutBox.js
+++ src/commons/components/LayoutBox.js
@@ -5,7 +5,7 @@ export const OuterBox = styled.div`
   max-width: 1400px;
   min-width: 1000px;
   padding: 50px;
-  margin: 50px auto;
+  margin: 30px auto;
 `;
 
 export const DetailsOuterBox = styled.div`
